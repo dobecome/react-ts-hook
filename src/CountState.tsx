c824f83ae707850741bd48d521af705b548b0bb1
Steve <sylvius4@gmail.com>
2022-03-30 14:35:15 +0900

    react with typescript

--- src/CountState.tsx
+++ src/CountState.tsx
@@ -1,14 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-function Count() {
-    const [count, setCount] = useState(0);
-
-    return (<div>
-        <p>clicked {count} times</p>
-        <button onClick={() => {
-            setCount(count + 1)
-        }}>Click</button>
-    </div>);
+interface CountStateProps {
+    count: number;
+    text: string;
 }
 
-export default Count;
+const CountState = (props : CountStateProps) => {
+    const [count, setCount] = useState(0);
+    useEffect(()=>{
+
+    }, [props.text])
+
+    return (
+        <div>
+            <p>clicked {count} times</p>
+            <button onClick={() => {
+                setCount(props.count)
+            }}>initial</button>
+            <button onClick={() => {
+                setCount(d => d + 1)
+            }}>plus</button>
+            <button onClick={() => {
+                setCount(count - 1)
+            }}>minus</button>
+        </div>
+    );
+}
+
+export default CountState;
